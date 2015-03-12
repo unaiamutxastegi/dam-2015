9fbbff6c25bd58184763fd4d308937444759064a
unaiamutxastegi <unai.amutxastegi@gmail.com>
2015-03-12 19:27:54 +0100

    Proyecto IONIC

--- AngularJS/Ej01_To_Do/assets/js/app.js
+++ AngularJS/Ej01_To_Do/assets/js/app.js
@@ -1,15 +1,27 @@
-var todoApp = angular.module('todoApp',[]);
+var todoApp = angular.module('todoApp', []);
 
-todoApp.controller('TodoController',['$scope', function($scope){
-	var todos = [];
+todoApp.controller('TodoController', ['$scope', function($scope) {
+    var todos = [];
 
-	todos.push({done: false, task: 'Aprender Angular JS'});
-	todos.push({done: false, task: 'Aprender Ionic'});
-	todos.push({done: true, task: 'Aprender JS'});
+    todos.push({
+        done: false,
+        task: 'Aprender Angular JS'
+    });
+    todos.push({
+        done: false,
+        task: 'Aprender Ionic'
+    });
+    todos.push({
+        done: true,
+        task: 'Aprender JS'
+    });
 
-	$scope.todos = todos;
+    $scope.todos = todos;
 
-	$scope.addTodo =function(){
-		todos.push({done: false, task: $scope.imputText});
-	};
+    $scope.addTodo = function() {
+        todos.push({
+            done: false,
+            task: $scope.imputText
+        });
+    };
 }]);
